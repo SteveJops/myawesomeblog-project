4a64ca7bd2a4d4725d5ecbae5c0734e66ec90409
SteveJops <steven07kolesiko@gmail.com>
2020-12-19 17:11:13 +0200

    added urls to events app made some new settings

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class BlogModel(models.Model):
     blog_title = models.CharField(max_length=300)
-    blog_date = models.DateTimeField(auto_now=True)
+    blog_date = models.DateTimeField(auto_now=False)
     blog_text = models.TextField()
     blog_image = models.ImageField(upload_to='blog_images/')
